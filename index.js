1125ab42e1cee7da44ef3db947ad8c4cf82a5de3
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2018-08-12 01:32:47 +0200

    []

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const express = require("express"), app = express()
+const express = require("express")
+const app = express();
+const bodyParser = require('body-parser')
 
 console.log("Server is activate")
 
+app.use(bodyParser.json())
+
 app.get("/", (req, res) => {
-	res.send("<h1>Welcome To Node.js Chat Boot</h1>");
+	res.send("<h1>Welcome To Node.js Chat Boot</h1>")
+});
+
+app.get("/facebook-webhook", (req, res) => {
+    let challenge = req.query['hub.challenge']
+    res.send(challenge)
+});
+
+app.post("/facebook-webhook", (req, res) => {
+	let messageBody = req.body
+    console.log(messageBody)
+    res.sendStatus(200)
 });
 
 app.listen(5000)
